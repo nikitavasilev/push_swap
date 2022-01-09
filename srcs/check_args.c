750fc1ee3357da088a6e2a6e2d425ed838c7b1aa
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-01-09 06:52:37 +0100

    args into line

--- srcs/check_args.c
+++ srcs/check_args.c
@@ -6,15 +6,39 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 18:40:40 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/01/09 01:39:34 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/01/09 06:47:31 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/push_swap.h"
+#include "push_swap.h"
+#include "libft.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static char	*parse_integers(int argc, char const *argv[])
+{
+	int		i;
+	char	*tmp;
+
+	tmp = ft_strdup("");
+	if (!tmp)
+		return (NULL);
+	i = 1;
+	while (i < argc)
+	{
+		tmp = ft_strjoin(tmp, argv[i]);
+		if (!tmp)
+			return (NULL);
+		i++;
+	}
+	return (tmp);
+}
 
 int	check_args(int argc, char const *argv[])
 {
 	int		i;
+	char	*args;
 	size_t	j;
 	t_list	*list;
 
@@ -26,10 +50,15 @@ int	check_args(int argc, char const *argv[])
 		j = 0;
 		while (argv[i][j])
 		{
-			if (!ft_isdigit(argv[i][j++]))
-				ft_error("Error");
+			if (!ft_isspace(argv[i][j]))
+				if (!ft_issign(argv[i][j]))
+					if (!ft_isdigit(argv[i][j]))
+						ft_error("Error");
+			j++;
 		}
 		i++;
 	}
+	args = parse_integers(argc, argv);
+	printf("%s\n", args);
 	return (1);
 }
