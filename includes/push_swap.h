8ac7b6c99445eacebe4644439bc1b46b5d4d68d2
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-16 18:54:21 +0100

    ++list + pos struct

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,20 +6,25 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 18:19:13 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/01/09 06:54:46 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/16 18:53:50 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct s_list	t_list;
-
-struct s_list
+typedef struct s_list
 {
-	char	*str;
-	t_list	*next;
-};
+	int				num;
+	struct s_list	*previous;
+	struct s_list	*next;
+}	t_list;
+
+typedef struct s_pos
+{
+	t_list	*head;
+	t_list	*tail;
+}	t_pos;
 
 int	check_args(int argc, char const *argv[]);
 
