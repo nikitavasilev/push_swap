c6d80906e9bd6538f99406055819119250cef9d9
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-16 18:42:10 +0100

    ++ atoi + overflow handle

---
+++ utils/atoi_err_overflow.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   atoi_err_overflow.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/16 18:35:19 by nvasilev          #+#    #+#             */
+/*   Updated: 2022/03/16 18:41:12 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <limits.h>
+#include <stdint.h>
+
+int	atoi_err_overflow(const char *str, int *err)
+{
+	__int64_t	nb;
+	__int64_t	max;
+	char		sign;
+
+	max = INT_MAX;
+	while ((ft_isspace(*str)))
+		str++;
+	sign = 1;
+	if (*str == '-' || *str == '+')
+		if (*(str++) == '-')
+			sign = -1;
+	nb = 0;
+	while (ft_isdigit(*str))
+	{
+		nb = nb * 10 + (*str - '0');
+		if (((int)nb < 0 && !((int)nb == INT_MIN && sign == -1))
+			|| (nb > (max + 1) && sign == -1))
+		{
+			*err = 1;
+			return (1);
+		}
+		str++;
+	}
+	return (nb * sign);
+}
