57876158707582ca46156525859c9e68cd3be128
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-16 18:56:33 +0100

    ++create_node()

---
+++ srcs/create_node.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_node.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/16 18:55:03 by nvasilev          #+#    #+#             */
+/*   Updated: 2022/03/16 18:55:38 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <stdlib.h>
+
+t_list	*create_node(t_list *previous, int num, t_pos *pos)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	list->num = num;
+	list->next = NULL;
+	list->previous = previous;
+	previous->next = list;
+	pos->tail = (void *)list;
+	return (list);
+}
