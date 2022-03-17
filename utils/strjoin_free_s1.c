1f5ec00669d6902c4740f6ace6532a6fb904a222
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-17 07:06:30 +0100

    ++free s1

---
+++ utils/strjoin_free_s1.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strjoin_free_s1.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 06:21:29 by nvasilev          #+#    #+#             */
+/*   Updated: 2022/03/17 06:22:05 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+char	*strjoin_free_s1(char *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (0);
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		str[i + ft_strlen(s1)] = s2[i];
+		i++;
+	}
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
+}
