a1492e0aa566941b936efc3f35e50ed66cca8cb1
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-17 08:00:55 +0100

    del tests + stack_a handling

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 18:25:43 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/17 06:53:13 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/17 07:33:09 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,16 @@
 
 int	main(int argc, char const *argv[])
 {
-	//t_pos	*stack_a;
+	t_pos	*stack_a;
 	char	**args;
-	size_t	i;
 
 	if (argc >= 2)
 	{
 		args = parse(argv);
-		i = 0;
-		while (args[i])
-		{
-			printf("%s\n", args[i]);
-			free(args[i]);
-			i++;
-		}
-		free(args);
-		//stack_a = fill_stack(args);
-		//print_list(stack_a);
-		//del_list(stack_a->head);
-		//free(stack_a);
+		stack_a = fill_stack(args);
+		print_list(stack_a);
+		del_list(stack_a->head);
+		free(stack_a);
 	}
 	else
 		ft_error("Usage: ./push_swap <integers>");
