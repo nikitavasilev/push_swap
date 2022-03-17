13f87ea506ee4ff4aa2ec8aca97570bc017184c0
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-17 07:02:59 +0100

    ++join

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 05:42:20 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/16 18:37:06 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/17 06:23:09 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ size_t		ft_strlen(const char *s);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strdup(const char *s1);
 int			atoi_err_overflow(const char *str, int *err);
+char		*strjoin_free_s1(char *s1, char const *s2);
 
 #endif
