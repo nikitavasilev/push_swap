32e2c4698a8e40a22403f4c04ad9ffe5841bb8a5
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-17 07:07:21 +0100

    test parse

--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,36 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 18:25:43 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/01/09 05:49:48 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/17 06:53:13 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int	main(int argc, char const *argv[])
 {
-	if (argc > 1)
+	//t_pos	*stack_a;
+	char	**args;
+	size_t	i;
+
+	if (argc >= 2)
 	{
-		if (check_args(argc, argv))
-			printf("yes\n");
+		args = parse(argv);
+		i = 0;
+		while (args[i])
+		{
+			printf("%s\n", args[i]);
+			free(args[i]);
+			i++;
+		}
+		free(args);
+		//stack_a = fill_stack(args);
+		//print_list(stack_a);
+		//del_list(stack_a->head);
+		//free(stack_a);
 	}
 	else
 		ft_error("Usage: ./push_swap <integers>");
