d82632942b9b0266a2095db459a5057e8ea74f80
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-17 07:58:26 +0100

    ++fill_stack()

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 18:19:13 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/17 06:42:18 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/17 07:35:18 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ typedef struct s_pos
 	t_list	*tail;
 }	t_pos;
 
-int		check_args(int argc, char const *argv[]);
 void	del_list(t_list *lst);
 t_list	*create_node(t_list *previous, int num, t_pos *pos);
 t_list	*alloc_lst(void);
@@ -36,6 +35,6 @@ void	rotate(t_pos *node);
 void	rrotate(t_pos *node);
 void	print_list(t_pos *pos);
 char	**parse(const char **argv);
-t_pos	*fill_stack(const char **args);
+t_pos	*fill_stack(char **args);
 
 #endif
