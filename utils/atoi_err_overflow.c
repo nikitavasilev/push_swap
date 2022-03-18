c3ae1350697f35dc73393bc4f0fbb7596d3b6411
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-18 04:35:40 +0100

    norm + rem + no_num handling

--- utils/atoi_err_overflow.c
+++ utils/atoi_err_overflow.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 18:35:19 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/17 06:24:21 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/18 04:32:10 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,53 @@
 #include <limits.h>
 #include <stdint.h>
 
+static int	err_check(const char c, int *err, int end)
+{
+	if ((c && !ft_isdigit(c) && end) || (c == '\0' && !end))
+	{
+		*err = 1;
+		return (1);
+	}
+	return (0);
+}
+
+static char	*conv_overflow(const char *str, char sign, int *err, __int64_t *nb)
+{
+	int	max;
+
+	max = INT_MAX;
+	*nb = 0;
+	while (*str && ft_isdigit(*str))
+	{
+		*nb = *nb * 10 + (*str - '0');
+		if (((int)(*nb) < 0 && !((int)(*nb) == INT_MIN && sign == -1))
+			|| (*nb > (max + 1) && sign == -1))
+		{
+			*err = 1;
+			break ;
+		}
+		str++;
+	}
+	return ((char *)str);
+}
+
 int	atoi_err_overflow(const char *str, int *err)
 {
 	__int64_t	nb;
-	__int64_t	max;
 	char		sign;
 
-	max = INT_MAX;
 	while ((ft_isspace(*str)))
 		str++;
 	sign = 1;
 	if (*str == '-' || *str == '+')
 		if (*(str++) == '-')
 			sign = -1;
-	nb = 0;
-	while (ft_isdigit(*str))
-	{
-		nb = nb * 10 + (*str - '0');
-		if (((int)nb < 0 && !((int)nb == INT_MIN && sign == -1))
-			|| (nb > (max + 1) && sign == -1))
-		{
-			*err = 1;
-			return (1);
-		}
-		str++;
-	}
-	if (!ft_isdigit(*str) && *str)
-	{
-		*err = 1;
+	if (err_check(*str, err, 0))
 		return (1);
-	}
+	str = conv_overflow(str, sign, err, &nb);
+	if (*err)
+		return (1);
+	if (err_check(*str, err, 1))
+		return (1);
 	return (nb * sign);
 }
