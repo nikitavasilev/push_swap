3d9d535ae84365007eb6b3e9c789d67a62bf2016
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-22 12:13:30 +0100

    ++ stack_b

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 18:25:43 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/22 11:24:21 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/22 12:08:40 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,30 @@
 #include "libft.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int	main(int argc, char const *argv[])
 {
 	t_pos	*stack_a;
+	t_pos	*stack_b;
 	char	**args;
 
 	if (argc >= 2)
 	{
 		args = parse(argv);
 		stack_a = fill_stack(args);
+		stack_b = alloc_pos();
+		printf("Stack a:\n");
+		print_list(stack_a);
+		push(stack_a, stack_b);
+		printf("Stack b:\n");
+		print_list(stack_b);
+		printf("Stack a:\n");
 		print_list(stack_a);
 		del_list(stack_a->head);
+		del_list(stack_b->head);
 		free(stack_a);
+		free(stack_b);
 	}
 	return (0);
 }
