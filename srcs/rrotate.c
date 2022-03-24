1b69fa8b4d6e08337e521933f94988cd8f82d2bf
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-24 20:05:29 +0100

    ++rrr()

--- srcs/rrotate.c
+++ srcs/rrotate.c
@@ -6,14 +6,15 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 19:59:09 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/16 19:59:34 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/24 19:57:44 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "push_swap.h"
-#include <stddef.h>
+#include <unistd.h>
 
-void	rrotate(t_pos *node)
+static void	rrotate(t_pos *node)
 {
 	t_list	*temp;
 
@@ -24,3 +25,23 @@ void	rrotate(t_pos *node)
 	node->head->previous = NULL;
 	node->tail = temp;
 }
+
+void	rra(t_pos *stack_a)
+{
+	rrotate(stack_a);
+	ft_putstr_fd("rra\n", STDOUT_FILENO);
+}
+
+void	rrb(t_pos *stack_b)
+{
+	rrotate(stack_b);
+	ft_putstr_fd("rrb\n", STDOUT_FILENO);
+}
+
+void	rrr(t_pos *stack_a, t_pos *stack_b)
+{
+	rrotate(stack_a);
+	rrotate(stack_b);
+	ft_putstr_fd("rrr\n", STDOUT_FILENO);
+}
+
