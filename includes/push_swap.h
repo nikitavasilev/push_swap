0da6b60c618a1e705c22405dd8250e5835432820
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-24 20:04:14 +0100

    ++ss(), ++rr(), ++rrr()

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 18:19:13 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/24 19:56:01 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/24 20:02:40 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,30 @@ typedef struct s_pos
 	t_list	*tail;
 }	t_pos;
 
-void	del_list(t_list *lst);
-t_list	*create_node(t_list *previous, int num, t_pos *pos);
 t_list	*alloc_lst(void);
 t_pos	*alloc_pos(void);
-void	sa(t_pos *stack_a);
-void	sb(t_pos *stack_b);
-void	ra(t_pos *stack_a);
-void	rb(t_pos *stack_b);
-void	rra(t_pos *stack_a);
-void	rrb(t_pos *stack_b);
-void	pa(t_pos *stack_b, t_pos *stack_a);
-void	pb(t_pos *stack_a, t_pos *stack_b);
-void	print_list(t_pos *pos);
+t_list	*create_node(t_list *previous, int num, t_pos *pos);
+
 char	**parse(const char **argv);
 t_pos	*fill_stack(char **args);
 void	check_dups(t_pos *pos, int *err);
+
+void	sa(t_pos *stack_a);
+void	sb(t_pos *stack_b);
+void	ss(t_pos *stack_a, t_pos *stack_b);
+
+void	ra(t_pos *stack_a);
+void	rb(t_pos *stack_b);
+void	rr(t_pos *stack_a, t_pos *stack_b);
+
+void	rra(t_pos *stack_a);
+void	rrb(t_pos *stack_b);
+void	rrr(t_pos *stack_a, t_pos *stack_b);
+
+void	pa(t_pos *stack_b, t_pos *stack_a);
+void	pb(t_pos *stack_a, t_pos *stack_b);
+
+void	print_list(t_pos *pos);
+void	del_list(t_list *lst);
 
 #endif
