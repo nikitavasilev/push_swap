99d5a5d1fee05af8b5b6fca9dbf1e362b14f68a6
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-24 17:29:12 +0100

    fix leak stack_b

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 18:25:43 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/23 21:16:19 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/24 17:27:35 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ int	main(int argc, char const *argv[])
 	{
 		args = parse(argv);
 		stack_a = fill_stack(args);
-		stack_b = alloc_pos();
 		printf("Stack a:\n");
 		print_list(stack_a);
 		stack_b = alloc_pos();
