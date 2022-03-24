9e5ed2b72e92f2c2c071485bdc320a8232740d94
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-24 19:42:33 +0100

    fix STDOUT macro

--- srcs/push.c
+++ srcs/push.c
@@ -6,14 +6,15 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 11:45:40 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/23 21:24:15 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/24 19:41:47 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stddef.h>
+#include "libft.h"
+#include <unistd.h>
 
-void	push(t_pos *src, t_pos *dest)
+static void	push(t_pos *src, t_pos *dest)
 {
 	t_list	*temp[2];
 
@@ -26,4 +27,16 @@ void	push(t_pos *src, t_pos *dest)
 	dest->head->previous = NULL;
 	src->head = temp[0];
 	src->head->previous = NULL;
+}
+
+void	pa(t_pos *stack_b, t_pos *stack_a)
+{
+	ft_putstr_fd("pa\n", STDOUT_FILENO);
+	push(stack_b, stack_a);
+}
+
+void	pb(t_pos *stack_a, t_pos *stack_b)
+{
+	ft_putstr_fd("pb\n", STDOUT_FILENO);
+	push(stack_a, stack_b);
 }
