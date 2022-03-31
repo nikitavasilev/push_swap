d3e253a45e7b1352bae64df1be517e4a836edefa
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-31 08:11:21 +0200

    ++is_sorted()

---
+++ srcs/sort.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 08:02:52 by nvasilev          #+#    #+#             */
+/*   Updated: 2022/03/31 08:10:19 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	is_sorted(t_pos *stack)
+{
+	t_list	*lst;
+
+	lst = stack->head;
+	while (lst->next)
+	{
+		if (lst->data > lst->next->data)
+			return (0);
+		lst = lst->next;
+	}
+	return (1);
+}
+
+void	sort(t_pos *stack_a, t_pos *stack_b)
+{
+	if (is_sorted(stack_a))
+		return ;
+}
