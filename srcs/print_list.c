63d1c16a322e05e6ec4b883e0119c8657b190b7c
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-31 06:18:53 +0200

    del printf

--- srcs/print_list.c
+++ srcs/print_list.c
@@ -6,26 +6,50 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 20:05:38 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/31 04:33:36 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/31 06:02:21 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
+#include <unistd.h>
+#include "libft.h"
+
+static void	print_line_info(char stack, int size, int data)
+{
+	if (stack == 'a' || stack == 'b')
+	{
+		ft_putstr_fd("\nStack ", STDOUT_FILENO);
+		if (stack == 'a')
+			ft_putchar_fd('a', STDOUT_FILENO);
+		else
+			ft_putchar_fd('b', STDOUT_FILENO);
+		ft_putstr_fd(" of size ", STDOUT_FILENO);
+		ft_putnbr_fd(size, STDOUT_FILENO);
+		ft_putstr_fd(":\n", STDOUT_FILENO);
+	}
+	else
+	{
+		ft_putstr_fd("Node no ", STDOUT_FILENO);
+		ft_putnbr_fd(size, STDOUT_FILENO);
+		ft_putstr_fd(": ", STDOUT_FILENO);
+		ft_putnbr_fd(data, STDOUT_FILENO);
+		ft_putchar_fd('\n', STDOUT_FILENO);
+	}
+}
 
 void	print_list(t_pos *pos, char stack)
 {
 	t_list	*current;
-	size_t	i;
+	int		i;
 
 	i = 0;
 	current = pos->head;
-	printf("\nStack %c of size %zu:\n", stack, pos->size);
+	print_line_info(stack, pos->size, 0);
 	while (current != pos->tail)
 	{
-		printf("Node no %zu: %d\n", i++, current->num);
+		print_line_info(' ', i++, current->num);
 		current = current->next;
 	}
 	if (pos->tail)
-		printf("Node no %zu: %d\n", i++, current->num);
+		print_line_info(' ', i++, current->num);
 }
