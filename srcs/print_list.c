0d353e032e9cc9bfca4f90eddecd115fecd4a2a2
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-31 04:35:38 +0200

    print change

--- srcs/print_list.c
+++ srcs/print_list.c
@@ -6,20 +6,21 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 20:05:38 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/23 21:18:11 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/31 04:33:36 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-void	print_list(t_pos *pos)
+void	print_list(t_pos *pos, char stack)
 {
 	t_list	*current;
 	size_t	i;
 
 	i = 0;
 	current = pos->head;
+	printf("\nStack %c of size %zu:\n", stack, pos->size);
 	while (current != pos->tail)
 	{
 		printf("Node no %zu: %d\n", i++, current->num);
