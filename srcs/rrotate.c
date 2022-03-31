6b9337797edfa31c4da9288f6ccc92d7d7e34819
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-31 03:28:49 +0200

    fix leak: 2nd->previous->head

--- srcs/rrotate.c
+++ srcs/rrotate.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 19:59:09 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/24 19:57:44 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/31 03:27:44 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,30 @@
 
 static void	rrotate(t_pos *node)
 {
-	t_list	*temp;
+	t_list	*old_head;
+	t_list	*old_tail;
 
-	temp = node->tail->previous;
-	node->tail->next = node->head;
-	node->tail->previous->next = NULL;
-	node->head = node->tail;
+	old_head = node->head;
+	old_tail = node->tail;
+	node->tail = old_tail->previous;
+	node->head = old_tail;
+	node->tail->next = NULL;
+	node->head->next = old_head;
 	node->head->previous = NULL;
-	node->tail = temp;
+	old_head->previous = node->head;
 }
 
 void	rra(t_pos *stack_a)
 {
-	rrotate(stack_a);
+	if (stack_a->size > 1)
+		rrotate(stack_a);
 	ft_putstr_fd("rra\n", STDOUT_FILENO);
 }
 
 void	rrb(t_pos *stack_b)
 {
-	rrotate(stack_b);
+	if (stack_b->size > 1)
+		rrotate(stack_b);
 	ft_putstr_fd("rrb\n", STDOUT_FILENO);
 }
 
@@ -44,4 +49,3 @@ void	rrr(t_pos *stack_a, t_pos *stack_b)
 	rrotate(stack_b);
 	ft_putstr_fd("rrr\n", STDOUT_FILENO);
 }
-
