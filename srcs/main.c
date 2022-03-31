034b5b384b283dbf8be1e78c2d3907b72352021c
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-03-31 04:37:13 +0200

    argc fix

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,12 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 18:25:43 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/03/28 22:20:40 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/03/31 04:36:27 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 int	main(int argc, char const *argv[])
@@ -21,20 +20,13 @@ int	main(int argc, char const *argv[])
 	t_pos	*stack_b;
 	char	**args;
 
-	if (argc > 2)
+	if (argc >= 2)
 	{
 		args = parse(argv);
 		stack_a = fill_stack(args);
-		printf("Stack a:\n");
-		print_list(stack_a);
 		stack_b = alloc_pos();
-		pb(stack_a, stack_b);
-		pb(stack_a, stack_b);
-		pb(stack_a, stack_b);
-		printf("Stack b:\n");
-		print_list(stack_b);
-		printf("Stack a:\n");
-		print_list(stack_a);
+		print_list(stack_a, 'a');
+		print_list(stack_b, 'b');
 		del_list(stack_a->head);
 		del_list(stack_b->head);
 		free(stack_a);
