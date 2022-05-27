53a390491bd2f11052f8a8127c2eb2dc195b70b8
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-27 07:06:46 +0200

    pb all except median

--- push_swap_srcs/insertion_sort.c
+++ push_swap_srcs/insertion_sort.c
@@ -6,10 +6,12 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 02:47:55 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/05/27 02:50:57 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/27 07:05:08 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
 #include "push_swap.h"
 
 static void	push_to_b(t_pos *stack_a, t_pos *stack_b, int median)
@@ -19,9 +21,13 @@ static void	push_to_b(t_pos *stack_a, t_pos *stack_b, int median)
 	current = stack_a->head;
 	while (current)
 	{
+		if (stack_a->size == 1)
+			return ;
 		if (current->data != median)
 			pb(stack_a, stack_b);
-		current = current->next;
+		else
+			ra(stack_a);
+		current = stack_a->head;
 	}
 }
 
@@ -29,7 +35,8 @@ void	insertion_sort(t_pos *stack_a, t_pos *stack_b, int median)
 {
 	t_list	*current;
 
+	printf("median = %d\n", median);
 	push_to_b(stack_a, stack_b, median);
 	current = stack_b->head;
-
+	print_stacks(stack_a, stack_b);
 }
