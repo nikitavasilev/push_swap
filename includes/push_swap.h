2618a74ad939c68068148bab40547bf4957d3404
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-27 02:56:17 +0200

    from static to global protos

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 18:19:13 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/04/06 15:48:22 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/27 02:48:28 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,33 +36,42 @@ typedef struct s_values
 	int	median;
 }	t_values;
 
-t_list	*alloc_lst(void);
-t_pos	*alloc_pos(void);
-t_list	*create_node(t_list *prev, int data, t_pos *pos);
+t_list		*alloc_lst(void);
+t_pos		*alloc_pos(void);
+t_list		*create_node(t_list *prev, int data, t_pos *pos);
 
-char	**parse(const char **argv);
-t_pos	*fill_stack(char **args);
-void	check_dups(t_pos *pos, int *err);
+char		**parse(const char **argv);
+t_pos		*fill_stack(char **args);
+void		check_dups(t_pos *pos, int *err);
 
-void	sa(t_pos *stack_a);
-void	sb(t_pos *stack_b);
-void	ss(t_pos *stack_a, t_pos *stack_b);
+void		sa(t_pos *stack_a);
+void		sb(t_pos *stack_b);
+void		ss(t_pos *stack_a, t_pos *stack_b);
 
-void	ra(t_pos *stack_a);
-void	rb(t_pos *stack_b);
-void	rr(t_pos *stack_a, t_pos *stack_b);
+void		ra(t_pos *stack_a);
+void		rb(t_pos *stack_b);
+void		rr(t_pos *stack_a, t_pos *stack_b);
 
-void	rra(t_pos *stack_a);
-void	rrb(t_pos *stack_b);
-void	rrr(t_pos *stack_a, t_pos *stack_b);
+void		rra(t_pos *stack_a);
+void		rrb(t_pos *stack_b);
+void		rrr(t_pos *stack_a, t_pos *stack_b);
 
-void	pa(t_pos *stack_b, t_pos *stack_a);
-void	pb(t_pos *stack_a, t_pos *stack_b);
+void		pa(t_pos *stack_b, t_pos *stack_a);
+void		pb(t_pos *stack_a, t_pos *stack_b);
 
-void	print_stacks(t_pos *stack_a, t_pos *stack_b);
-void	free_stacks(t_pos *stack_a, t_pos *stack_b);
-void	del_list(t_list *lst);
+void		print_stacks(t_pos *stack_a, t_pos *stack_b);
+void		free_stacks(t_pos *stack_a, t_pos *stack_b);
+void		del_list(t_list *lst);
 
-int		sort(t_pos *stack_a, t_pos *stack_b);
+int			sort(t_pos *stack_a, t_pos *stack_b);
+int			bubble_sort(int *array, unsigned int size);
+t_values	*find_median(t_pos *stack_a);
+void		selection_sort(t_pos *stack_a, t_pos *stack_b);
+void		insertion_sort(t_pos *stack_a, t_pos *stack_b, int median);
+int			is_sorted(t_pos *stack);
+int			is_rev_sorted(t_pos *stack);
+int			smart_swap_a(t_pos *stack_a);
+int			find_min(t_pos *stack);
+size_t		find_shortest_path(size_t index, size_t size, t_pos *stack);
 
 #endif
