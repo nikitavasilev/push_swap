9316e26931734987a8ee340d8710b0bcde3afd4b
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-06-06 09:43:26 +0200

    ++find_shortest_path_b

--- push_swap_srcs/find_shortest_path.c
+++ push_swap_srcs/find_shortest_path.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 02:46:52 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/05/27 02:47:06 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/06/06 06:47:00 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,27 @@ size_t	find_shortest_path(size_t index, size_t size, t_pos *stack)
 		while (index > 0)
 		{
 			ra(stack);
+			index--;
+		}
+	}
+	return (index);
+}
+
+size_t	find_shortest_path_b(size_t index, size_t size, t_pos *stack)
+{
+	if (index > size / 2)
+	{
+		while (index < size)
+		{
+			rrb(stack);
+			index++;
+		}
+	}
+	else
+	{
+		while (index > 0)
+		{
+			rb(stack);
 			index--;
 		}
 	}
