73d8a7154569081fc1f468eea2f7feb48afa5386
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-06-06 09:45:10 +0200

    ++partial sort

--- push_swap_srcs/sort.c
+++ push_swap_srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 08:02:52 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/05/27 02:49:14 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/06/06 04:35:20 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@
 
 int	sort(t_pos *stack_a, t_pos *stack_b)
 {
-	t_values	*a_values;
+	/*t_values	*a_values;
 
 	a_values = find_median(stack_a);
 	if (!a_values)
-		return (0);
+		return (0);*/
 	(void)stack_b;
 	if (is_sorted(stack_a))
 	{
-		free(a_values);
+		//free(a_values);
 		return (1);
 	}
-	if (stack_a->size <= 10)
+	if (stack_a->size <= 50)
 		selection_sort(stack_a, stack_b);
 	else
-		insertion_sort(stack_a, stack_b, a_values->median);
-	free(a_values);
+		partial_sort(stack_a, stack_b);
+	//free(a_values);
 	return (1);
 }
