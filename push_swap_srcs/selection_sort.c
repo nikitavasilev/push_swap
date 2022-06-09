d884d0cc30acb85da2ef5902265a9d7c980c9186
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-06-09 17:36:13 +0200

    small select_sort + cpyies + optimized sort test

--- push_swap_srcs/selection_sort.c
+++ push_swap_srcs/selection_sort.c
@@ -6,13 +6,13 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 02:35:44 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/06/06 06:52:15 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/06/07 05:36:14 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	selection_sort(t_pos *stack_a, t_pos *stack_b)
+void	selection_sort_small(t_pos *stack_a, t_pos *stack_b)
 {
 	size_t	i;
 	int		min;
@@ -53,6 +53,70 @@ void	selection_sort(t_pos *stack_a, t_pos *stack_b)
 	if (stack_b->size > 0)
 		while (stack_b->head)
 			pa(stack_b, stack_a);
+}
+
+void	selection_sort(t_pos *stack_a, t_pos *stack_b)
+{
+	size_t	i;
+	int		min;
+	int		max;
+	t_list	*current;
+
+	current = stack_a->head;
+	min = find_min(stack_a);
+	max = find_max(stack_a);
+	i = 0;
+	while (current)
+	{
+		if (current->data == min)
+		{
+			i = find_shortest_path(i, stack_a->size, stack_a);
+			if (!is_sorted(stack_a))
+				pb(stack_a, stack_b);
+			if (stack_a->size > 1 && !is_sorted(stack_a))
+			{
+				i = 0;
+				min = find_min(stack_a);
+				current = stack_a->head;
+				continue ;
+			}
+		}
+		if (current->data == max)
+		{
+			i = find_shortest_path(i, stack_a->size, stack_a);
+			if (!is_sorted(stack_a))
+			{
+				pb(stack_a, stack_b);
+				rb(stack_b);
+			}
+			if (stack_a->size > 1 && !is_sorted(stack_a))
+			{
+				i = 0;
+				max = find_max(stack_a);
+				current = stack_a->head;
+				continue ;
+			}
+		}
+		if (stack_a->size == 1)
+			pb(stack_a, stack_b);
+		current = current->next;
+		i++;
+	}
+	if (stack_b->size > 0)
+	{
+		max = find_max(stack_b);
+		while (stack_b->head)
+		{
+			if (stack_b->head && stack_b->head->data == max)
+			{
+				pa(stack_b, stack_a);
+				if (stack_b->size > 0)
+					max = find_max(stack_b);
+			}
+			else
+				rb(stack_b);
+		}
+	}
 }
 
 void	selection_sort_invert(t_pos *stack_a, t_pos *stack_b)
@@ -69,9 +133,8 @@ void	selection_sort_invert(t_pos *stack_a, t_pos *stack_b)
 		if (current->data == max)
 		{
 			i = find_shortest_path_b(i, stack_b->size, stack_b);
-			if (!is_sorted(stack_b))
-				pa(stack_b, stack_a);
-			if (stack_b->size > 1 && !is_sorted(stack_b))
+			pa(stack_b, stack_a);
+			if (stack_b->size > 0)
 			{
 				i = 0;
 				max = find_max(stack_b);
@@ -82,9 +145,6 @@ void	selection_sort_invert(t_pos *stack_a, t_pos *stack_b)
 		current = current->next;
 		i++;
 	}
-	if (stack_b->size > 0)
-		while (stack_b->head)
-			pa(stack_b, stack_a);
 }
 
 void	selection_sort_stop(t_pos *stack_a, t_pos *stack_b)
@@ -92,25 +152,12 @@ void	selection_sort_stop(t_pos *stack_a, t_pos *stack_b)
 	size_t	i;
 	int		min;
 	t_list	*current;
-	size_t	init_size_stack_b;
 
-	init_size_stack_b = stack_b->size;
 	current = stack_a->head;
 	min = find_min(stack_a);
 	i = 0;
 	while (current)
 	{
-		if (stack_a->size == 3 && is_rev_sorted(stack_a))
-		{
-			ra(stack_a);
-			sa(stack_a);
-			continue ;
-		}
-		if (i == 0 && stack_a->size >= 3 && !is_sorted(stack_a))
-		{
-			if (smart_swap_a(stack_a))
-				current = stack_a->head;
-		}
 		if (current->data == min)
 		{
 			i = find_shortest_path(i, stack_a->size, stack_a);
@@ -127,7 +174,54 @@ void	selection_sort_stop(t_pos *stack_a, t_pos *stack_b)
 		current = current->next;
 		i++;
 	}
-	if (stack_b->size > 0)
-		while (stack_b->size > init_size_stack_b)
-			pa(stack_b, stack_a);
+}
+
+void	selection_sort_stop_min_max(t_pos *stack_a, t_pos *stack_b)
+{
+	size_t	i;
+	int		min;
+	int		max;
+	t_list	*current;
+
+	current = stack_a->head;
+	min = find_min(stack_a);
+	max = find_max(stack_a);
+	i = 0;
+	while (current)
+	{
+		if (current->data == min)
+		{
+			i = find_shortest_path(i, stack_a->size, stack_a);
+			if (!is_sorted(stack_a))
+				pb(stack_a, stack_b);
+			if (stack_a->size > 1 && !is_sorted(stack_a))
+			{
+				i = 0;
+				min = find_min(stack_a);
+				current = stack_a->head;
+				continue ;
+			}
+		}
+		if (current->data == max)
+		{
+			i = find_shortest_path(i, stack_a->size, stack_a);
+			if (!is_sorted(stack_a))
+			{
+				pb(stack_a, stack_b);
+				rb(stack_b);
+			}
+			if (stack_a->size > 1 && !is_sorted(stack_a))
+			{
+				i = 0;
+				max = find_max(stack_a);
+				current = stack_a->head;
+				continue ;
+			}
+		}
+		current = current->next;
+		i++;
+	}
+	if (stack_a->size > 0)
+		while (stack_a->head)
+			pb(stack_a, stack_b);
 }
