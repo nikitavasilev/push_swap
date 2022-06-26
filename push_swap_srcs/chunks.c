e8f0f73b24b26aa365b853b2dab6c8a29b822f6f
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-06-27 01:57:09 +0200

    dispatched to fix

--- push_swap_srcs/chunks.c
+++ push_swap_srcs/chunks.c
@@ -6,331 +6,141 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 02:45:55 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/06/07 09:49:50 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/06/27 01:50:47 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 #include "push_swap.h"
 
-static int	*get_array(t_pos *stack_a)
-{
-	int		*array;
-	t_list	*current;
-	int		i;
-
-	i = 0;
-	array = (int *)malloc(sizeof(int) * stack_a->size);
-	if (!array)
-		return (NULL);
-	current = stack_a->head;
-	while (current)
-	{
-		array[i] = current->data;
-		current = current->next;
-		i++;
-	}
-	return (array);
-}
-
-float	power(float x, int y)
-{
-	float	res;
-
-	res = 1;
-	while (y)
-	{
-		res *= x;
-		y--;
-	}
-	return (res);
-}
-
-t_chunks	*get_chunks(t_pos *stack_a)
-{
-	int				*array;
-	t_chunks		*chunks;
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	chunk_size;
-
-	if (stack_a->size > 100)
-		chunk_size = 20 * power(1.2, stack_a->size / 100) + 1;
-	else
-		chunk_size = stack_a->size * 0.2;
-	array = get_array(stack_a);
-	if (!array)
-		return (0);
-	chunks = (t_chunks *)malloc(sizeof(t_chunks));
-	if (!chunks)
-	{
-		free(array);
-		return (0);
-	}
-	chunks->count = stack_a->size / chunk_size;
-	chunks->data = malloc(sizeof(int) * chunks->count);
-	if (!chunks->data)
-	{
-		free(array);
-		free(chunks);
-		return (0);
-	}
-	bubble_sort(array, stack_a->size);
-	i = 0;
-	j = chunk_size;
-	while (i < chunks->count)
-	{
-		chunks->data[i] = array[j - 1];
-		i++;
-		j += chunk_size - (chunk_size * 0.1);
-	}
-	free(array);
-	return (chunks);
-}
-
-int	calc_moves_to_up(size_t index, size_t size)
-{
-	int	ret;
-
-	ret = 0;
-	if (index > size / 2)
-	{
-		while (index < size)
-		{
-			index++;
-			ret++;
-		}
-	}
-	else
-	{
-		while (index > 0)
-		{
-			index--;
-			ret++;
-		}
-	}
-	return (ret);
-}
-
-int	find_cheapest(t_pos *stack, size_t size, int range)
+static int	how_many_elements(t_pos *stack, int range)
 {
 	t_list	*current;
-	size_t	i;
-	int		temp;
-	int		moves;
-	int		cheapest;
+	int		count;
 
-	i = 0;
-	moves = INT_MAX;
+	count = 0;
 	current = stack->head;
 	while (current)
 	{
 		if (current->data <= range)
-		{
-			temp = calc_moves_to_up(i, size);
-			if (temp < moves)
-			{
-				moves = temp;
-				cheapest = current->data;
-			}
-		}
+			count++;
 		current = current->next;
-		i++;
 	}
-	return (cheapest);
+	return (count);
 }
 
-int	steps_to_tail(t_pos *stack, int range)
-{
-	t_list	*current;
-	int		steps;
+// static void	p_curr_chk(t_pos *st_a, int *el, t_chunks *chunks, size_t *i)
+// {
+// 	(*el)--;
+// 	if (*el == 0)
+// 	{
+// 		(*i)++;
+// 		*el = how_many_elements(st_a, chunks->data[*i]);
+// 	}
+// }
 
-	steps = 0;
-	current = stack->head;
-	while (current)
-	{
-		if (current->data <= range)
-			return (steps);
-		current = current->next;
-		steps++;
-	}
-	return (steps);
-}
+// static void	p_nxt_chk(t_pos *st_a, t_pos *st_b, t_list **curr, int next_data)
+// {
+// 	(*curr) = (*curr)->next;
+// 	pb(st_a, st_b);
+// 	if ((*curr)->data > next_data)
+// 		rr(st_a, st_b);
+// 	else
+// 		rb(st_b);
+// 	(*curr) = st_a->head;
+// }
 
-int	steps_to_head(t_pos *stack, int range)
-{
-	t_list	*current;
-	int		steps;
+// static int	rotate_or_break(t_pos *stack_a, t_list **curr, int el_to_push)
+// {
+// 	if (stack_a->size > 1 && el_to_push)
+// 	{
+// 		ra(stack_a);
+// 		(*curr) = stack_a->head;
+// 	}
+// 	if (!el_to_push)
+// 		return (0);
+// 	return (1);
+// }
 
-	steps = 0;
-	current = stack->tail;
-	while (current)
-	{
-		if (current->data <= range)
-			return (steps);
-		current = current->prev;
-		steps++;
-	}
-	return (steps);
-}
+// void	push_chunks_to_b(t_pos *stack_a, t_pos *stack_b, t_chunks *chunks)
+// {
+// 	size_t	i;
+// 	t_list	*current;
+// 	int		el_to_push;
+
+// 	i = 0;
+// 	current = stack_a->head;
+// 	el_to_push = how_many_elements(stack_a, chunks->data[i]);
+// 	while (current)
+// 	{
+// 		if (i < chunks->count && current->data <= chunks->data[i])
+// 		{
+// 			pb(stack_a, stack_b);
+// 			p_curr_chk(stack_a, &el_to_push, chunks, &i);
+// 			current = stack_a->head;
+// 			continue ;
+// 		}
+// 		if (i + 1 < chunks->count && current->data > chunks->data[i]
+// 			&& current->data <= chunks->data[i + 1])
+// 		{
+// 			p_nxt_chk(stack_a, stack_b, &current, chunks->data[i + 1]);
+// 			continue ;
+// 		}
+// 		if (!rotate_or_break(stack_a, &current, el_to_push))
+// 			break ;
+// 	}
+// }
 
 void	push_chunks_to_b(t_pos *stack_a, t_pos *stack_b, t_chunks *chunks)
 {
-	t_list	*current;
-	size_t	i;
 	size_t	j;
-	int		up;
+	t_list	*current;
+	int		el_to_push;
 
-	i = 0;
 	j = 0;
+	el_to_push = how_many_elements(stack_a, chunks->data[j]);
 	current = stack_a->head;
-	if (steps_to_head(stack_a, chunks->data[j]) <
-		steps_to_tail(stack_a, chunks->data[j]))
-		up = 1;
-	else
-		up = 0;
 	while (current)
 	{
-		if (j + 1 < chunks->count && i == 0)
-		{
-			if (current->data > chunks->data[j] && current->data <= chunks->data[j + 1])
-			{
-				pb(stack_a, stack_b);
-				rb(stack_b);
-				i = 0;
-				current = stack_a->head;
-				continue ;
-			}
-		}
 		if (j < chunks->count && current->data <= chunks->data[j])
 		{
-			if (!up)
+			pb(stack_a, stack_b);
+			current = stack_a->head;
+			el_to_push--;
+			if (el_to_push == 0)
 			{
-				while (i > 0)
-				{
-					ra(stack_a);
-					current = stack_a->head;
-					if (j + 1 < chunks->count)
-					{
-						if (current->data > chunks->data[j] && current->data <= chunks->data[j + 1])
-						{
-							pb(stack_a, stack_b);
-							rb(stack_b);
-							i = 0;
-							current = stack_a->head;
-							break ;
-						}
-					}
-					i--;
-				}
+				j++;
+				el_to_push = how_many_elements(stack_a, chunks->data[j]);
 			}
-			else if (up)
-			{
-				while (i < stack_a->size)
-				{
-					rra(stack_a);
-					current = stack_a->head;
-					if (j + 1 < chunks->count)
-					{
-						if (current->data > chunks->data[j] && current->data <= chunks->data[j + 1])
-						{
-							pb(stack_a, stack_b);
-							rb(stack_b);
-							i = 0;
-							current = stack_a->head;
-							break ;
-						}
-					}
-					i++;
-				}
-			}
-			if (j < chunks->count && current->data <= chunks->data[j])
-				pb(stack_a, stack_b);
-			i = 0;
+			continue ;
+		}
+		if (j + 1 < chunks->count && current->data > chunks->data[j]
+			&& current->data <= chunks->data[j + 1])
+		{
+			current = current->next;
+			pb(stack_a, stack_b);
+			if (current->data > chunks->data[j + 1])
+				rr(stack_a, stack_b);
+			else
+				rb(stack_b);
 			current = stack_a->head;
 			continue ;
 		}
 		if (current == stack_a->tail && j < chunks->count)
 		{
 			current = stack_a->head;
-			i = 0;
-			j++;
-			if (steps_to_head(stack_a, chunks->data[j]) <
-				steps_to_tail(stack_a, chunks->data[j]))
-				up = 1;
-			else
-				up = 0;
 			continue ;
 		}
-		current = current->next;
-		i++;
+		if (stack_a->size > 1 && el_to_push)
+		{
+			ra(stack_a);
+			current = stack_a->head;
+		}
+		if (!el_to_push && j + 1 < chunks->count)
+		{
+			j++;
+			el_to_push = how_many_elements(stack_a, chunks->data[j]);
+		}
+		if (!el_to_push)
+			break ;
 	}
-}
-
-// void	push_chunks_to_b(t_pos *stack_a, t_pos *stack_b, t_chunks *chunks)
-// {
-// 	t_list	*current;
-// 	size_t	i;
-// 	size_t	j;
-// 	int		cheapest;
-
-// 	i = 0;
-// 	j = 0;
-// 	current = stack_a->head;
-// 	while (current)
-// 	{
-// 		if (j + 1 < chunks->count && i == 0)
-// 		{
-// 			if (current->data > chunks->data[j] && current->data <= chunks->data[j + 1])
-// 			{
-// 				pb(stack_a, stack_b);
-// 				rb(stack_b);
-// 				i = 0;
-// 				current = stack_a->head;
-// 				continue ;
-// 			}
-// 		}
-// 		if (j < chunks->count && current->data <= chunks->data[j])
-// 		{
-// 			cheapest = find_cheapest(stack_a, stack_a->size, chunks->data[j]);
-// 			if (current->data == cheapest)
-// 			{
-// 				i = find_shortest_path(i, stack_a->size, stack_a);
-// 				if (!is_sorted(stack_a))
-// 					pb(stack_a, stack_b);
-// 				i = 0;
-// 				current = stack_a->head;
-// 				continue ;
-// 			}
-// 		}
-// 		if (current == stack_a->tail && j < chunks->count)
-// 		{
-// 			current = stack_a->head;
-// 			i = 0;
-// 			j++;
-// 			continue ;
-// 		}
-// 		current = current->next;
-// 		i++;
-// 	}
-// }
-
-void	partial_sort(t_pos *stack_a, t_pos *stack_b)
-{
-	t_chunks	*chunks;
-
-	chunks = get_chunks(stack_a);
-	if (!chunks)
-		return ;
-	push_chunks_to_b(stack_a, stack_b, chunks);
-	free(chunks->data);
-	free(chunks);
-	if (stack_a->size <= 20)
-		selection_sort_stop(stack_a, stack_b);
-	else
-		selection_sort_stop_min_max(stack_a, stack_b);
-	selection_sort_invert(stack_a, stack_b);
-	// print_stacks(stack_a, stack_b);
 }
