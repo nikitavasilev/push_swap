b252f60f994dcf92b1bd7c03ad97679c534cb9cf
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-06-28 01:17:02 +0200

    norm + protection

--- push_swap_srcs/sort.c
+++ push_swap_srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 08:02:52 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/06/26 22:50:29 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/06/27 21:05:36 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@
 void	partial_sort(t_pos *stack_a, t_pos *stack_b)
 {
 	t_chunks	*chunks;
+	t_stacks	stacks;
 
+	stacks.a = stack_a;
+	stacks.b = stack_b;
 	chunks = get_chunks(stack_a);
 	if (!chunks)
 		return ;
@@ -27,7 +30,7 @@ void	partial_sort(t_pos *stack_a, t_pos *stack_b)
 		selection_sort_stop(stack_a, stack_b);
 	else
 		selection_sort_stop_min_max(stack_a, stack_b);
-	selection_sort_invert(stack_a, stack_b);
+	selection_sort_invert(stack_a, stack_b, &stacks);
 }
 
 int	sort(t_pos *stack_a, t_pos *stack_b)
@@ -38,5 +41,7 @@ int	sort(t_pos *stack_a, t_pos *stack_b)
 		selection_sort_small(stack_a, stack_b);
 	else
 		partial_sort(stack_a, stack_b);
-	return (1);
+	if (is_sorted(stack_a))
+		return (1);
+	return (0);
 }
